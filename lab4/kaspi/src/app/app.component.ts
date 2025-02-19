dbc7e6b3668b4bcfabcdb51b521716bdbb7b66df
Bekzat Saparbekov <bekzatsaparbekov070@gmail.com>
2025-02-19 16:27:00 +0500

    Update app.component.ts

--- lab4/kaspi/src/app/app.component.ts
+++ lab4/kaspi/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent, RouterModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<app-products></app-products>`, 
+  imports: [ProductsComponent],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kaspi';
+  title = 'kaspi-products';
 }
