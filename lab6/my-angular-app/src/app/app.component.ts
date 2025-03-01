36ed168b8ace9c618398a8793121593af0d1b5d0
Bekzat Saparbekov <bekzatsaparbekov070@gmail.com>
2025-03-01 15:06:43 +0500

    Update app.component.ts

--- lab6/my-angular-app/src/app/app.component.ts
+++ lab6/my-angular-app/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterModule]
+  imports: [CommonModule, RouterModule],
+  template: `
+    <h1>My Angular App</h1>
+    <nav>
+      <a routerLink="/home">Home</a> |
+      <a routerLink="/about">About</a> |
+      <a routerLink="/albums">Albums</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Album Manager';
+  title = 'angular-albums-app';
 }
