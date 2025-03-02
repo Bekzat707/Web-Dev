f9a97048934e2d30cca95cc93aafe85e4b3a02e5
Bekzat Saparbekov <bekzatsaparbekov070@gmail.com>
2025-03-02 17:18:52 +0500

    Update app.routes.ts

--- lab6/my-angular-app/src/app/app.routes.ts
+++ lab6/my-angular-app/src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AlbumsComponent } from './components/albums/albums.component';
-import { AlbumDetailComponent } from './components/album-detail/album-detail.component';
-import { AlbumPhotosComponent } from './components/album-photos/album-photos.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { PhotosComponent } from './album-photos/album-photos.component';
+import { AppComponent } from './app.component';  
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'albums', component: AlbumsComponent },
-  { path: 'albums/:id', component: AlbumDetailComponent },
-  { path: 'albums/:id/photos', component: AlbumPhotosComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'albums', component: AlbumComponent },
+    { path: 'albums/:id', component: AlbumDetailComponent },
+    { path: 'albums/:id/photos', component: PhotosComponent },
+    { path: '**', redirectTo: '' } 
 ];
