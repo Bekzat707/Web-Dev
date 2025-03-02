4040bf1d339c8e603d58446bb22cd61185609dac
Bekzat Saparbekov <bekzatsaparbekov070@gmail.com>
2025-03-02 17:17:37 +0500

    Update albums.service.ts

--- lab6/my-angular-app/src/app/services/albums.service.ts
+++ lab6/my-angular-app/src/app/services/albums.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Album {
+  id: number;
+  title: string;
+}
+
+export interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AlbumsService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/albums';
+  private albumsUrl = 'assets/albums.json';
+  private photosUrl = 'assets/photos.json';
 
   constructor(private http: HttpClient) {}
 
-  getAlbums(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getAlbums(): Observable<Album[]> {
+    return this.http.get<Album[]>(this.albumsUrl);
   }
 
-  createAlbum(album: { title: string }): Observable<any> {
-    return this.http.post<any>(this.apiUrl, album);
+  getAlbumById(id: number): Observable<Album | undefined> {
+    return this.http.get<Album[]>(this.albumsUrl).pipe(
+      map((albums: Album[]) => albums.find((album: Album) => album.id === id))
+    );
   }
 
-  deleteAlbum(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
-  }
-  getAlbum(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
-  }
-
-  updateAlbum(id: number, album: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, album);
+  getPhotosByAlbumId(albumId: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>(this.photosUrl).pipe(
+      map((photos: Photo[]) => photos.filter((photo: Photo) => photo.albumId === albumId))
+    );
   }
 }
